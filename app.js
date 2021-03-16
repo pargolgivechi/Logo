98a7a03e913b14708954eec6928a1bd3f1b20c71
pargolgivechi <pargolgivechi@gmail.com>
2021-03-17 00:08:00 +0100

    completed ad

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const text = document.querySelector('.text');
+const main = document.querySelector('.main-text');
 
 setTimeout(function () {
   text.style.opacity = "1";
   text.style.transition = "opacity 2s";
 }, 2000);
+setTimeout(function () {
+  text.style.opacity = "0";
+  text.style.transition = "opacity 2s";
+}, 5000);
+setTimeout(function () {
+  main.style.display = "block";
+}, 1000);
+setTimeout(function () {
+  main.style.opacity = "0";
+  main.style.transition = "opacity 1s";
+}, 7000);
